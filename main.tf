2cac3dd18250861ab4386c8c2970a93b74d11976
Ramakrishna875549 <ramakrishna875549@gmail.com>
2026-07-20 18:59:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,4 +13,7 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   }
