971af5f38598601843cf169860c773306468b755
Ramakrishna875549 <ramakrishna875549@gmail.com>
2026-07-21 15:00:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,15 @@ variable "location" {
 variable "resource_group_name" {
   default = "aks-rg"
 }
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+  default     = "aks-vnet"
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the VNet"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
