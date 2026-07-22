580e1c6654a3bbfc0ec854c0127b1a947c814089
Ramakrishna875549 <ramakrishna875549@gmail.com>
2026-07-22 10:47:56 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
 output "resource_group" {
   value = azurerm_resource_group.rg.name
 }
+
+output "adf_id" {
+  value = azurerm_data_factory.adf.id
+}
+
+output "adf_name" {
+  value = azurerm_data_factory.adf.name
+}
+
+output "adf_identity_principal_id" {
+  description = "System-assigned managed identity principal ID, useful for granting RBAC access to storage/key vault"
+  value       = azurerm_data_factory.adf.identity[0].principal_id
+}
