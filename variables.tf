ccfd2d844fe90cad163eab095e6b5def3ce9354b
Ramakrishna875549 <ramakrishna875549@gmail.com>
2026-07-22 10:43:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,3 +19,33 @@ variable "vnet_address_space" {
   type        = list(string)
   default     = ["10.0.0.0/16"]
 }
+
+variable "adf_name" {
+  description = "Name of the Azure Data Factory"
+  type        = string
+  default     = "adf-project"
+}
+
+variable "adf_managed_vnet_enabled" {
+  description = "Enable managed virtual network for ADF"
+  type        = bool
+  default     = false
+}
+
+variable "adf_public_network_enabled" {
+  description = "Allow public network access to ADF"
+  type        = bool
+  default     = true
+}
+
+variable "adf_github_config" {
+  description = "Optional GitHub integration for ADF (leave null to skip)"
+  type = object({
+    account_name    = string
+    branch_name     = string
+    git_url         = string
+    repository_name = string
+    root_folder     = string
+  })
+  default = null
+}
