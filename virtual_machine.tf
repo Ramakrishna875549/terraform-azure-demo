9c5d388ead8becbb96db94eec57fc0d1a8a3c7e1
Ramakrishna875549 <ramakrishna875549@gmail.com>
2026-07-23 10:25:17 +0530

    Update virtual_machine.tf

--- virtual_machine.tf
+++ virtual_machine.tf
@@ -68,17 +68,17 @@ resource "azurerm_network_security_group" "shir_nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-security_rule {
-  name                       = "AllowOutboundHTTPS"
-  priority                   = 100
-  direction                  = "Outbound"
-  access                     = "Allow"
-  protocol                   = "Tcp"
-  source_port_range          = "*"
-  destination_port_range     = "3389"
-  source_address_prefix      = "${chomp(data.http.my_ip.response_body)}/32"
-  destination_address_prefix = "*"
-}
+  security_rule {
+    name                       = "AllowRDPFromMyIP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "${chomp(data.http.my_ip.response_body)}/32"
+    destination_address_prefix = "*"
+  }
 
   security_rule {
     name                       = "AllowOutboundHTTPS"
@@ -103,8 +103,6 @@ security_rule {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-
 }
 
 resource "azurerm_network_interface_security_group_association" "shir_nic_nsg" {
